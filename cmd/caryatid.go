ce86808143bd6f0468ad071216a5e3ce6c327b9f
Micah R Ledbetter <me@micahrl.com>
2016-11-29 12:12:32 -0600

    WIP-idk

--- cmd/caryatid.go
+++ cmd/caryatid.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"os"
@@ -8,13 +9,24 @@ import (
 	"strings"
 )
 
-func main() {
-	extension := flag.String("extension", ".box", "The file extension of the artifact. Passing a file with any other extension will cause the script to fail. For use with packer and vagrant this must be 'box'. (This option is intended mostly for testing.)")
-	testing := flag.Bool("testing", false, "Pass this flag to parse the command line but nothing else. Used in testing, which sees an os.Exit(0) and an exit value of anything else as a failure.")
+type ParsedArguments struct {
+	CommandInvocation string
+	CommandName string
+	BoxName string
+	BoxDescription string
+	BoxVersion string
+	BoxProvider string
+	ArtifactPath string
+	Backend string
+	Destination string
+}
+
+func ParseArguments(arguments ...string) (parsed ParsedArguments, err error) {
+	os.Args = arguments
+
+	extension := flag.String("extension", ".box", "The file extension of the artifact. Passing a file with any other extension will cause the script to fail. For use with packer and vagrant this must be '.box'. (This option is intended mostly for testing.)")
 	flag.Parse()
 
-	cmdInvocation := os.Args[0]
-	cmdName := path.Base(cmdInvocation)
 	reqArgCount := 7
 	allowedBackends := []string{"copy", "scp"}
 
@@ -23,42 +35,37 @@ func main() {
 		return false
 	}
 
-	if len(flag.Args()) != reqArgCount {
-		fmt.Println(fmt.Sprintf("%v args were required but %v args were passed", reqArgCount, len(flag.Args())))
-		os.Exit(1)
+	if flag.NArg() != reqArgCount {
+		err = errors.New(fmt.Sprintf("%v args were required but %v args were passed", reqArgCount, len(flag.Args())))
+		return
 	}
 
-	var (
-		name = flag.Arg(0)
-		description = flag.Arg(1)
-		version = flag.Arg(2)
-		provider = flag.Arg(3)
-		artifact = flag.Arg(4)
-		backend = flag.Arg(5)
-		destination = flag.Arg(6)
-	)
+	parsed = ParsedArguments{os.Args[0], path.Base(os.Args[0]), flag.Arg(0), flag.Arg(1), flag.Arg(2), flag.Arg(3), flag.Arg(4), flag.Arg(5), flag.Arg(6)}
+	fmt.Println(os.Args[0])
+	fmt.Println(path.Base(os.Args[0]))
 
-	if !(backendIsAllowed(backend)) {
-		fmt.Println(fmt.Sprintf("Passed a backend of '%v', which is not valid; backends must be one of '%v'", backend, allowedBackends))
-		os.Exit(1)
+	if !(backendIsAllowed(parsed.Backend)) {
+		err = errors.New(fmt.Sprintf("Passed a backend of '%v', which is not valid; backends must be one of '%v'", parsed.Backend, allowedBackends))
+		return
 	}
-	if !(strings.HasSuffix(artifact, *extension)) {
-		fmt.Println(fmt.Sprintf("Passed an artifact of '%v', which does not have the required extension '%v'", artifact, *extension))
-		os.Exit(1)
+	if !(strings.HasSuffix(parsed.ArtifactPath, *extension)) {
+		err = errors.New(fmt.Sprintf("Passed an artifact of '%v', which does not have the required extension '%v'", parsed.ArtifactPath, *extension))
+		return
 	}
 
-	fmt.Println("Command invocation:", cmdInvocation)
-	fmt.Println("Command name:", cmdName)
-	fmt.Println("Box name:", name)
-	fmt.Println("Box description:", description)
-	fmt.Println("Box version:", version)
-	fmt.Println("Box provider:", provider)
-	fmt.Println("Artifact path:", artifact)
-	fmt.Println("Backend:", backend)
-	fmt.Println("Destination directory:", destination)
+	return
+}
 
-	if !*testing {
-		fmt.Println("We don't do anything yet, so exit here unless we're running the tests")
+func main() {
+	pa, err := ParseArguments(os.Args...)
+	if err != nil {
+		fmt.Println(fmt.Sprintf("ERROR: %v", err))
 		os.Exit(1)
 	}
+	fmt.Println(pa)
+
+	// if !*testing {
+	// 	fmt.Println("We don't do anything yet, so exit here unless we're running the tests")
+	// 	os.Exit(1)
+	// }
 }
--- cmd/caryatid_test.go
+++ cmd/caryatid_test.go
@@ -2,14 +2,40 @@ package main
 
 import (
 	"fmt"
-	"os"
+	// "os"
+	"path"
 	"testing"
 )
 
-func TestMain(t *testing.T) {
-	fmt.Println("Hiiiiiii")
+func TestArgumentParsing(t *testing.T) {
+	var (
+		cmdInv = "/some/path/to/caryatid.test"
+		_, cmdName = path.Split(cmdInv)
+		boxName = "ExampleBox"
+		boxDesc = "This is an example box"
+		boxVers = "1.2.3"
+		boxProv = "virtualbox"
+		artifact = fmt.Sprintf("%v_%v_%v.box", boxName, boxVers, boxProv)
+		backend = "copy"
+		dest = "/some/path/somewhere"
+	)
+	var (
+		parsed ParsedArguments
+		err error
+	)
 
-	// NOTE: running it without the -testing=true flag should fail
-	os.Args = []string{"/dummy/path/to/executable", "-testing=true", "1", "2", "3", "4", "5.box", "copy", "7"}
-	main()
+	parsed, err = ParseArguments(cmdInv, boxName, boxDesc, boxVers, boxProv, artifact, backend, dest)
+	if err != nil {t.Fatal(fmt.Sprintf("ParseArguments failed with error %v", err))}
+	if parsed.CommandInvocation != cmdInv {t.Fatal(fmt.Sprintf("Expected .CommandInvocation to be '%v' but it was '%v'", cmdInv, parsed.CommandInvocation))}
+	if parsed.CommandName != cmdName {t.Fatal(fmt.Sprintf("Expected .CommandName to be '%v' but it was '%v'", cmdName, parsed.CommandName))}
+	if parsed.BoxName != boxName {t.Fatal(fmt.Sprintf("Expected .BoxName to be '%v' but it was '%v'", boxName, parsed.BoxName))}
+	if parsed.BoxDescription != boxDesc {t.Fatal(fmt.Sprintf("Expected .BoxDescription to be '%v' but it was '%v'", boxDesc, parsed.BoxDescription))}
+	if parsed.BoxVersion != boxVers {t.Fatal(fmt.Sprintf("Expected .BoxVersion to be '%v' but it was '%v'", boxVers, parsed.BoxVersion))}
+	if parsed.BoxProvider != boxProv {t.Fatal(fmt.Sprintf("Expected .BoxProvider to be '%v' but it was '%v'", boxProv, parsed.BoxProvider))}
+	if parsed.ArtifactPath != artifact {t.Fatal(fmt.Sprintf("Expected .ArtifactPath to be '%v' but it was '%v'", artifact, parsed.ArtifactPath))}
+	if parsed.Backend != backend {t.Fatal(fmt.Sprintf("Expected .Backend to be '%v' but it was '%v'", backend, parsed.Backend))}
+	if parsed.Destination != dest {t.Fatal(fmt.Sprintf("Expected .Destination to be '%v' but it was '%v'", dest, parsed.Destination))}
+
+	// This blows up, I think because flag.Parse() can't be run twice, but ParseArguments calls it
+	// parsed, err = ParseArguments(cmdInv, boxName, boxDesc, boxVers, boxProv, artifact, backend, dest)
 }
