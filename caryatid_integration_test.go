675fa3d2f8f6637b1d91edcc4321c8f3102d1528
Micah R Ledbetter <me@micahrl.com>
2017-02-07 20:32:27 -0600

    Add flag to keep integration_tests dir after tests finish

--- caryatid_integration_test.go
+++ caryatid_integration_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -11,41 +12,57 @@ import (
 	"github.com/mitchellh/packer/packer"
 )
 
+// Integration tests
+// NOTE: Call as 'go test -k' to keep the integration_tests directory around after the tests finish... otherwise it will be deleted
+
+const integrationTestDirName = "integration_tests"
+
+var (
+	_, thisfile, _, runtimeCallerOk = runtime.Caller(0)
+	thisdir, _                      = path.Split(thisfile)
+	integrationTestDir              = path.Join(thisdir, integrationTestDirName)
+	keepFlag                        = flag.Bool("k", false, fmt.Sprintf("Keep the %v directory after running integration tests", integrationTestDir))
+)
+
+// A somewhat silly test that exists because we can't put logic outside of a function
+func TestThatIntegrationTestingIsSetUpCorrectly(t *testing.T) {
+	if !runtimeCallerOk {
+		t.Fatal("Failed to detect thisdir using runtime.Caller()")
+	}
+	fmt.Println(fmt.Sprintf("Detected running the test directory as '%v'", thisdir))
+}
+
 func TestPostProcess(t *testing.T) {
 	var (
 		err error
 	)
 
-	_, thisfile, _, ok := runtime.Caller(0)
-	if !ok {
-		t.Fatal("Failed to detect thisdir using runtime.Caller()")
-	}
-	thisdir, _ := path.Split(thisfile)
-	fmt.Println(fmt.Sprintf("Detected running the test directory as '%v'", thisdir))
-	testdir := path.Join(thisdir, "integration_test")
-
 	testBoxName := "TestBoxName"
 	testArtifactContents := "This is a test artifact"
 	testProviderName := "TestProvider"
 	testArtifactFilename := fmt.Sprintf("%v_%v.box", testBoxName, testProviderName)
-	testArtifactPath := path.Join(testdir, testArtifactFilename)
+	testArtifactPath := path.Join(integrationTestDir, testArtifactFilename)
 	ui := &packer.BasicUi{}
 	inartifact := &packer.MockArtifact{FilesValue: []string{testArtifactPath}}
 	pp := CaryatidPostProcessor{}
 	inkeepinput := false
 
-	pp.config.CatalogRoot = fmt.Sprintf("file://%v", testdir)
+	pp.config.CatalogRoot = fmt.Sprintf("file://%v", integrationTestDir)
 	pp.config.Description = "Test box description"
 	pp.config.KeepInputArtifact = inkeepinput
 	pp.config.Name = testBoxName
 	pp.config.Version = "6.6.6"
 
 	// Set up test: write files etc
-	err = os.MkdirAll(testdir, 0700)
+	err = os.MkdirAll(integrationTestDir, 0700)
 	if err != nil {
 		t.Fatal("Error trying to create test directory: ", err)
 	}
-	defer os.RemoveAll(testdir)
+	if *keepFlag {
+		fmt.Printf("Will not remove integraion test dir '%v' after tests complete", integrationTestDir)
+	} else {
+		defer os.RemoveAll(integrationTestDir)
+	}
 
 	err = ioutil.WriteFile(testArtifactPath, []byte(testArtifactContents), 0600)
 	if err != nil {
@@ -71,7 +88,7 @@ func TestPostProcess(t *testing.T) {
 	// }
 
 	expectedCatalogData := `{"name":"TestBoxName","description":"Test box description","versions":[{"version":"6.6.6","providers":[{"name":"TestProvider","url":"file:///Users/mrled/Documents/Go/src/github.com/mrled/packer-post-processor-caryatid/integration_test/TestBoxName/TestBoxName_6.6.6_TestProvider.box","checksum_type":"sha1","checksum":"78bc8a542fa84494ff14ae412196d134c603960c"}]}]}`
-	testCatalogPath := path.Join(testdir, fmt.Sprintf("%v.json", testBoxName))
+	testCatalogPath := path.Join(integrationTestDir, fmt.Sprintf("%v.json", testBoxName))
 	testCatalogData, err := ioutil.ReadFile(testCatalogPath)
 	if err != nil {
 		t.Fatal("Error trying to read the test catalog: ", err)
@@ -84,7 +101,7 @@ func TestPostProcess(t *testing.T) {
 	if err != nil {
 		t.Fatal("Failed to calculate sha1sum of ", testArtifactPath)
 	}
-	copiedBoxPath := path.Join(testdir, testBoxName, fmt.Sprintf("%v_%v_%v.box", testBoxName, pp.config.Version, testProviderName))
+	copiedBoxPath := path.Join(integrationTestDir, testBoxName, fmt.Sprintf("%v_%v_%v.box", testBoxName, pp.config.Version, testProviderName))
 	copiedDigest, err := sha1sum(copiedBoxPath)
 	if err != nil {
 		t.Fatal("Failed to calculate sha1sum of ", copiedBoxPath)
