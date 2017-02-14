f270e699d4c353e6b07c2f0b7613e8f665a0de30
Micah R Ledbetter <me@micahrl.com>
2017-02-13 20:54:56 -0600

    Generate zipfile w/ binary + readme for each platform

--- buildrelease.go
+++ buildrelease.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"archive/zip"
 	"bytes"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"path"
@@ -35,12 +38,107 @@ func updateEnv(inEnv []string, name string, value string) (outEnv []string, err 
 	return
 }
 
+func getTempFilePath(directory string) (tempFilePath string, err error) {
+	tempFile, err := ioutil.TempFile(directory, "")
+	if err != nil {
+		return
+	}
+	tempFilePath = tempFile.Name()
+	tempFile.Close()
+	err = os.Remove(tempFilePath)
+	return
+}
+
+func assembleZip(goos string, goarch string, thisDir string, zipOutDir string, zipBaseName string) (err error) {
+	zipOutPath := path.Join(zipOutDir, fmt.Sprintf("%v.zip", zipBaseName))
+	srcDir := path.Join(thisDir, "packer-post-processor-caryatid")
+	readmePath := path.Join(thisDir, "readme.markdown")
+
+	exeName := "packer-post-processor-caryatid"
+	if goos == "windows" {
+		exeName = fmt.Sprintf("%v.exe", exeName)
+	}
+
+	tmpExeName, err := getTempFilePath(thisDir)
+	if err != nil {
+		return
+	}
+
+	cmd := exec.Command("go", "build", "-o", tmpExeName)
+	cmd.Dir = srcDir
+
+	environment := os.Environ()
+	environment, err = updateEnv(environment, "GOARCH", goarch)
+	if err != nil {
+		return
+	}
+	environment, err = updateEnv(environment, "GOOS", goos)
+	if err != nil {
+		return
+	}
+	cmd.Env = environment
+
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	fmt.Printf("Building %v-%v binary\n", goos, goarch)
+	err = cmd.Run()
+	defer os.Remove(tmpExeName)
+	if err != nil {
+		return fmt.Errorf("Error running command '%v':\nSTDOUT: %v\nSTDERR: %v\nGo error: %v\n", cmd, stdout.String(), stderr.String(), err)
+	}
+
+	fmt.Printf("Creating zipfile for %v-%v binary at %v\n", goos, goarch, zipOutPath)
+	zipOutFile, err := os.Create(zipOutPath)
+	defer zipOutFile.Close()
+	if err != nil {
+		return
+	}
+	zipWriter := zip.NewWriter(zipOutFile)
+	defer zipWriter.Close()
+
+	zExeFile, err := zipWriter.Create(fmt.Sprintf("%v/%v", zipBaseName, exeName))
+	if err != nil {
+		return
+	}
+
+	fsExeFile, err := os.Open(tmpExeName)
+	defer fsExeFile.Close()
+	if err != nil {
+		return
+	}
+
+	_, err = io.Copy(zExeFile, fsExeFile)
+	if err != nil {
+		return
+	}
+
+	zReadmeFile, err := zipWriter.Create(fmt.Sprintf("%v/readme.markdown", zipBaseName))
+	if err != nil {
+		return
+	}
+
+	fsReadmeFile, err := os.Open(readmePath)
+	defer fsReadmeFile.Close()
+	if err != nil {
+		return
+	}
+
+	_, err = io.Copy(zReadmeFile, fsReadmeFile)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
 func main() {
 	var (
 		err                  error
 		_, thisFile, _, rcOk = runtime.Caller(0)
 		thisDir, _           = path.Split(thisFile)
-		ppDir                = path.Join(thisDir, "packer-post-processor-caryatid")
 		releaseDir           = path.Join(thisDir, "release")
 	)
 
@@ -49,54 +147,30 @@ func main() {
 	}
 
 	platforms := [][]string{
-		[]string{"amd64", "darwin"},
-		[]string{"amd64", "freebsd"},
-		[]string{"386", "freebsd"},
-		[]string{"arm", "freebsd"},
-		[]string{"amd64", "linux"},
-		[]string{"386", "linux"},
-		[]string{"arm", "linux"},
-		[]string{"amd64", "windows"},
-		[]string{"386", "windows"},
+		[]string{"darwin", "amd64"},
+		[]string{"freebsd", "amd64"},
+		[]string{"freebsd", "386"},
+		[]string{"freebsd", "arm"},
+		[]string{"linux", "amd64"},
+		[]string{"linux", "386"},
+		[]string{"linux", "arm"},
+		[]string{"windows", "amd64"},
+		[]string{"windows", "386"},
+	}
+
+	err = os.MkdirAll(releaseDir, 0777)
+	if err != nil {
+		panic(err)
 	}
 
 	for _, plat := range platforms {
-		goarch := plat[0]
-		goos := plat[1]
-		platOutDir := path.Join(releaseDir, goos, goarch)
+		goos := plat[0]
+		goarch := plat[1]
+		zipBaseName := fmt.Sprintf("caryatid_%v-%v", goos, goarch)
 
-		filename := "packer-post-processor-caryatid"
-		if goos == "windows" {
-			filename = fmt.Sprintf("%v.exe", filename)
-		}
-
-		if err = os.MkdirAll(platOutDir, 0777); err != nil {
-			panic(err)
-		}
-
-		cmd := exec.Command("go", "build", "-o", path.Join(platOutDir, filename))
-		cmd.Dir = ppDir
-
-		environment := os.Environ()
-		environment, err = updateEnv(environment, "GOARCH", goarch)
+		err = assembleZip(goos, goarch, thisDir, releaseDir, zipBaseName)
 		if err != nil {
-			panic(err)
-		}
-		environment, err = updateEnv(environment, "GOOS", goos)
-		if err != nil {
-			panic(err)
-		}
-		cmd.Env = environment
-
-		var stdout bytes.Buffer
-		var stderr bytes.Buffer
-
-		cmd.Stdout = &stdout
-		cmd.Stderr = &stderr
-
-		err := cmd.Run()
-		if err != nil {
-			panic(fmt.Errorf("Error running command '%v':\nSTDOUT: %v\nSTDERR: %v\nGo error: %v\n", cmd, stdout.String(), stderr.String(), err))
+			fmt.Printf("Error assembling zip file for %v-%v: %v", goos, goarch, err)
 		}
 	}
 }
